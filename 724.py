064e71825fe281e69f54d8fd9f14e15cc3d410ea
frixc7 <frixc7@gmail.com>
2025-04-07 05:42:38 +0100

    Update 724.py

--- 724.py
+++ 724.py
@@ -9,3 +9,20 @@ class Solution:
             elif sum(nums[:i]) == sum(nums[i+1:]):
                 return i
         return -1
+
+# SOL2: with Prefix Sums
+
+class Solution:
+    def pivotIndex(self, nums: List[int]) -> int:
+        prefixsums = []
+        t = 0
+        for i in nums:
+            t += i
+            prefixsums.append(t)
+
+        for i in range(len(nums)):
+            leftsum = prefixsums[i-1] if i-1 >= 0 else 0
+            rightsum = (prefixsums[-1] - prefixsums[i]) if i != len(nums)-1 else 0
+            if rightsum == leftsum:
+                return i
+        return -1
