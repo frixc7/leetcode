45fae6e676e0823fc62ae52406bcd5d6839a1313
frixc7 <frixc7@gmail.com>
2025-04-09 05:14:43 +0100

    Update 242.py

--- 242.py
+++ 242.py
@@ -1,3 +1,5 @@
+# SOLUTION 1
+
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         hashtable1 = {}
@@ -9,3 +11,19 @@ class Solution:
             hashtable2[c] = t.count(c)
 
         return True if hashtable1 == hashtable2 else False
+
+# SOLUTION 2
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+
+        hashtable = {}
+        for c in set(s):
+            hashtable[c] = s.count(c)
+        
+        for c in set(t):
+            if c not in hashtable or hashtable[c] != t.count(c):
+                return False
+
+        return True
